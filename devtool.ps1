58477ca513924506d2d52f6db01860880176addd
Glenn Lundberg <glenn.lundberg@crepido.com>
2016-01-22 15:39:39 +0100

    minor fixes for auto login

--- devtool.ps1
+++ devtool.ps1
@@ -4,26 +4,32 @@
 
 function Login()
 {
-    $profileSettingsFile = Get-Item (join-path (Get-Location)  "glenn.azureprofile")
-    if($profileSettingsFile.Exists)
+    $settingsFilePath = join-path (Get-Location)  "glenn.azureprofile"
+    $settingsFileExists = Test-Path $settingsFilePath
+    
+    if($settingsFileExists)
     {
         Write-Host "Found settingsfile, logging in"
-        $account = Select-AzureRmProfile -path $profileSettingsFile.FullName
-        Write-Host "$($account.Context.Account) logged in..."
-        
-        $subscription = Get-AzureRmSubscription
-        Write-Host "Active subscription: $($subscription.SubscriptionName) ($($subscription.SubscriptionId))"
+        $account = Select-AzureRmProfile -path $settingsFilePath
+        Write-Host "$($account.Context.Account) logged in..."                                           
     }
     else
-    {
-        Add-AzureRmAccount   
+    {        
+        Add-AzureRmAccount                        
+        Save-AzureRmProfile -Path $settingsFilePath                                 
     }    
+}
+
+function WriteAzureSubscriptionInfo()
+{                
+    $subscription = Get-AzureSubscription
+    Write-Host "Active subscription: $($subscription.SubscriptionName) ($($subscription.SubscriptionId))"
 }
 
 function ListVMs()
 {
     Write-Host "Found VMs:"
-    Write-Host (Get-AzureRmVM).Name
+    Write-Host (Get-AzureRmVm).Name
 }
 
 function ShutOff($vmName)
@@ -49,12 +55,13 @@ function SwitchOn($vmName)
     if($vm.Statuses | where Code -match "PowerState/deallocated")
     {
         Write-Host "$($vm.Name) is not running, starting..."
-        Start-AzureRmVM -Name $vmName -ResourceGroupName $vmName  
+        Start-AzureRmVM -Name $vmName -ResourceGroupName $vmName          
     }
     else
     {
         Write-Warning "$($vm.Name) is is already running"
     }
+    return $vmName
     
 }
 
@@ -65,12 +72,38 @@ function ConnectRDP($vmName, $user, $passWord)
 }
 
 
+function InstallAzureCommands()
+{
+   # Install the Azure Resource Manager modules from the PowerShell Gallery
+    Install-Module AzureRM
+    Install-AzureRM
+
+    # Install the Azure Service Management module from the PowerShell Gallery
+    Install-Module Azure
+
+    # Import AzureRM modules for the given version manifest in the AzureRM module
+    Import-AzureRM
+
+    # Import Azure Service Management module
+    Import-Module Azure
+}
+
+function IsAzureCommandsInstalled()
+{
+    Get-Module -ListAvailable | where -Match -Value "Azure" -Property "Name"
+}
     
 Write-Host "Devtool started"
+#InstallAzureCommands
 Login
-ListVMs
+#WriteAzureSubscriptionInfo 
+#ListVMs
 #ShutOff "dev"
 #SwitchOn "dev"
-ConnectRDP "dev" "Glenn" "password"
+#ConnectRDP "dev" "Glenn" "password"
+
+#ConnectRDP  $(SwitchOn "dev")  "Glenn"  "password"
+ShutOff "dev"
+ 
 
 
